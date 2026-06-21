06d1f883ae9ba74cb7300d5de0ed137073d88422
CNDY1390 <cndy1390@qq.com>
2026-06-21 09:25:28 +0800

    chore: configure bucket automation

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$Upstream = "CNDY1390/scoop-bucket-cndy:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
